a4b4e0245da75657519bbc794cdbe22858e17895
Tai Sakuma <tai.sakuma@gmail.com>
2024-11-13 10:46:54 -0500

    Add a test

--- tests/test_emitter.py
+++ tests/test_emitter.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 import time
 
 import pytest
@@ -61,6 +62,42 @@ async def test_emit_alert() -> None:
     async with nextline:
         event = asyncio.Event()
         await nextline.run_continue_and_wait(event)
+
+    # Assert the HTTP POST request
+    assert route.called
+    assert len(route.calls) == 1
+    call = route.calls[0]
+    assert url == call.request.url
+    data = json.loads(call.request.content)
+    labels = data['alerts'][0]['labels']
+    description = data['alerts'][0]['annotations']['description']
+    assert platform == labels['platform']
+    assert 'ValueError' in description
+
+
+@respx.mock
+async def test_emit_exception(caplog: pytest.LogCaptureFixture) -> None:
+    nextline = Nextline(func_raise)
+
+    url = 'http://localhost:5000/alerts'
+    platform = 'pytest'
+    emitter = Emitter(url=url, platform=platform)
+    assert nextline.register(emitter)
+
+    # Mock the HTTP POST request
+    route = respx.post(url).respond(status_code=500)
+
+    # Run a script that raises an exception and fails to emit an alert
+    with caplog.at_level(logging.ERROR):
+        async with nextline:
+            event = asyncio.Event()
+            await nextline.run_continue_and_wait(event)
+
+    # Assert the log message
+    records = [r for r in caplog.records if r.name == Emitter.__module__]
+    assert len(records) == 1
+    assert records[0].levelname == 'ERROR'
+    assert 'Failed to emit alert' in records[0].message
 
     # Assert the HTTP POST request
     assert route.called
