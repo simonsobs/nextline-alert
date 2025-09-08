2903303c38f9b27fb85f4e5dff5c07463b422cba
Tai Sakuma <tai.sakuma@gmail.com>
2025-09-08 10:51:00 -0400

    Move registration to `alerts/__init__.py`

--- src/nextline_alert/alerts/__init__.py
+++ src/nextline_alert/alerts/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ['AlertRunFailed']
+from nextline import Nextline
 
 from .run_failed import AlertRunFailed
+
+
+def register(nextline: Nextline, url: str, platform: str) -> None:
+    nextline.register(AlertRunFailed(url=url, platform=platform))
